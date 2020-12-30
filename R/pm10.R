1a9d23dbcbf234449b3efb29d6ca92f7ac4dbed6
raffaele-morelli <raffaele.morelli@isprambiente.it>
2020-12-30 09:43:57 +0100

    Fix in documentazione

--- R/pm10.R
+++ R/pm10.R
@@ -10,10 +10,10 @@
 #' 
 #' @format Un tibble con dati da gennaio 2016 a giugno 2020 con 5 variabili:
 #' \describe{
-#'   \item{reporting_year}{int anno della rilevazione}
-#'   \item{pollutant_fk}{int chiave esterna dell'inquinante} 
-#'   \item{station_eu_code}{char codice europeo della stazione}
-#'   \item{date}{date data della rilevazione}
-#'   \item{value}{dbl dato osservato}
+#'   \item{reporting_year}{Anno della rilevazione}
+#'   \item{pollutant_fk}{Chiave esterna dell'inquinante} 
+#'   \item{station_eu_code}{Codice europeo della stazione}
+#'   \item{date}{Data della rilevazione}
+#'   \item{value}{Dato osservato}
 #' }
 "pm10"
