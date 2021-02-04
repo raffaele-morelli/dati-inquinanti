ddeb5094a916ed723d7748bf11b201caf7b703c1
raffaele-morelli <raffaele.morelli@isprambiente.it>
2021-02-04 06:02:19 +0100

    Aggiunto CO

---
+++ R/co.R
@@ -0,0 +1,21 @@
+#' Dati giornalieri relativi al CO per tutte le stazioni sul territorio
+#' 
+#' Per ottenere i dati regionali degli inquinani è sufficiente utilizzare le
+#' funzionalità del pacchetto dplyr
+#' 
+#' @examples 
+#' # i dati relativi al CO della regione Piemonte  
+#' dplyr::filter(stazioniAria, region_id == 1) %>% inner_join(co, by = c("station_eu_code")) 
+#'
+#' # analogo al precedente ma usando una *ricerca* (match) sul campo 'regione'
+#' dplyr::filter(stazioniAria, grepl("piemonte", regione, ignore.case = TRUE)) %>% inner_join(co, by = c("station_eu_code")) 
+#' 
+#' @format Un tibble con dati da gennaio 2016 a giugno 2020 con 5 variabili:
+#' \describe{
+#'   \item{reporting_year}{Anno della rilevazione}
+#'   \item{pollutant_fk}{Chiave esterna dell'inquinante} 
+#'   \item{station_eu_code}{Codice europeo della stazione}
+#'   \item{date}{Data della rilevazione}
+#'   \item{value}{Dato osservato}
+#' }
+"co"
